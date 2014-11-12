d4653d7e448e6bbee0d209ce0d5f1ed1491e90df
AaronBClark <aaron_b_clark@yahoo.com>
2014-11-11 18:08:52 -0800

    Projects Layout
    Added basic content to projects.php
    Changed layout of projects.php
    Changed colors on about.php via jquery-ui.css
    Added header to contact.php

--- display/body/projects.php
+++ display/body/projects.php
@@ -1,17 +1,24 @@
 
-<style>
-	#projectsBody{
-		padding-top:85px;
-	}
-</style>
-<div class="pure-g" id="projectsBody">
-	<div class="pure-u-sm-3-24">
+
+<div class="pure-g" id="project">
+	<div class="pure-u-md-3-24 pure-u-1-24">
 	</div>
-	
-	<div class="pure-u-1 pure-u-sm-18-24">
-		middle column content
-	 </div>
-	 
-	 <div class="pure-u-1 pure-u-sm-3-24">
-	 </div>
+	<div class="pure-u-md-18-24 pure-u-22-24" id="centerdiv">
+		<hr style="margin-top: 100px;">
+		<h1>Projects</h1>
+		<div id="content">
+			<h3>Information Security Workshops</h3>
+			<p>We do stuff, yo!</p>
+		</div>
+		<div id="content">
+			<h3>ACM Programming Competition</h3>
+			<p>We did stuff there, too!</p>
+		</div>
+		<div id="content">
+			<h3>Webadvisor App</h3>
+			<p>We're going to do stuff!</p>
+		</div>
+	</div>
+	<div class="pure-u-md-3-24 pure-u-1-24">
+	</div>
 </div>
